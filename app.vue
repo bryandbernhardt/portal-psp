cddddb0d624cfae4593cec43854d04faa5c0b7a5
Bryan Dietrich Bernhardt <bryan@minhavisita.com.br>
2024-01-07 17:41:40 -0300

    feat(app): add meta head

--- app.vue
+++ app.vue
@@ -2,3 +2,14 @@
   <BaseHeader />
   <NuxtPage />
 </template>
+
+<script setup>
+useSeoMeta({
+  title: 'Portal PSP',
+  ogTitle: 'Portal PSP',
+  description: 'O seu portal mais completo e atualizado de PSP!',
+  ogDescription: 'O seu portal mais completo e atualizado de PSP!',
+  ogImage: '/assets/images/psp-white.webp',
+  twitterCard: '/assets/images/psp-white.webp',
+})
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,10 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+    }
+  },
 })
